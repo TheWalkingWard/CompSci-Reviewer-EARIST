91f2d38e9de3649f062ed16a0620d7a1d0220082
Jahred-Mario <jahredbaccay97@gmail.com>
2023-12-06 09:39:19 +0800

    Updated the Questions

---
+++ HumanComputerInteraction.js
@@ -0,0 +1,135 @@
+function Hint1() {
+    document.getElementById("Explain01").innerHTML = "Dragon Baller yun men";
+}
+
+function ExplainWrong1() {
+    document.getElementById("Explain01").innerHTML = "Wrong Answer\n The Correct is\n C. Application Software are a group of programs that run on electronic devices. These programs are designed for the user to carry out different tasks.";
+    document.getElementById("btn-changeRed1").className = "btn btn-danger";
+    document.getElementById("btn-changeRed01").className = "btn btn-danger";
+    document.getElementById("btn-changeRed001").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen1").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed1").disabled = true;
+    document.getElementById("btn-changeRed01").disabled = true;
+    document.getElementById("btn-changeRed001").disabled = true;
+    document.getElementById("btn-changeGreen1").disabled = true;
+    document.getElementById("Hint01").disabled = true;
+}
+
+function ExplainCorrect1() {
+    document.getElementById("Explain01").innerHTML = "C. Application Software are a group of programs that run on electronic devices. These programs are designed for the user to carry out different tasks.";
+    
+    document.getElementById("btn-changeGreen1").className = "btn btn-success";
+    document.getElementById("btn-changeRed1").className = "btn btn-danger";
+    document.getElementById("btn-changeRed01").className = "btn btn-danger";
+    document.getElementById("btn-changeRed001").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed1").disabled = true;
+    document.getElementById("btn-changeRed01").disabled = true;
+    document.getElementById("btn-changeRed001").disabled = true;
+    document.getElementById("btn-changeGreen1").disabled = true;
+    document.getElementById("Hint01").disabled = true;
+}
+
+
+// NUMBER 2
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Wrong Answer\n The Correct is\n";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain02").innerHTML = "Wrong Answer\n The Correct is\n B. Address a place where a person or organization may be communicated with.";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Address a place where a person or organization may be communicated with.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+//NUMBER 3
+function Hint2() {
+    document.getElementById("Explain03").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain03").innerHTML = "Wrong Answer\n The Correct is\n C. Python is often used as a support language for software developers, for build control and management, testing, and in many other ways.";
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
+    document.getElementById("Hint03").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain03").innerHTML = "C. Python is often used as a support language for software developers, for build control and management, testing, and in many other ways.";
+    
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+//NUMBER 4
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain02").innerHTML = "Wrong Answer\n The Correct is\n";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "Nice! Si Usog nga!";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
