3ae3eaa36b216bde0c830b015b87a22a8a5f6447
Jahred-Mario <jahredbaccay97@gmail.com>
2023-12-06 11:17:12 +0800

    Sub updated

---
+++ ApplicationDevelopment.js
@@ -0,0 +1,349 @@
+function Hint1() {
+    document.getElementById("Explain01").innerHTML = "Programs that run on electronic devices";
+}
+
+function ExplainWrong1() {
+    document.getElementById("Explain01").innerHTML = "No, the answer is A. New State A program is fetched from the secondary memory and kept in the job queue by the job scheduler, this state is called a new state.";
+
+    document.getElementById("btn-changeRed1").className = "btn btn-danger";
+    document.getElementById("btn-changeRed01").className = "btn btn-danger";
+    document.getElementById("btn-changeRed001").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen1").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed1").disabled = true;
+    document.getElementById("btn-changeRed01").disabled = true;
+    document.getElementById("btn-changeRed001").disabled = true;
+    document.getElementById("btn-changeGreen1").disabled = true;
+    document.getElementById("Hint01").disabled = true;
+}
+
+function ExplainCorrect1() {
+    document.getElementById("Explain01").innerHTML = "Yes! A. New State A program is fetched from the secondary memory and kept in the job queue by the job scheduler, this state is called a new state.";
+
+    document.getElementById("btn-changeGreen1").className = "btn btn-success";
+    document.getElementById("btn-changeRed1").className = "btn btn-danger";
+    document.getElementById("btn-changeRed01").className = "btn btn-danger";
+    document.getElementById("btn-changeRed001").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed1").disabled = true;
+    document.getElementById("btn-changeRed01").disabled = true;
+    document.getElementById("btn-changeRed001").disabled = true;
+    document.getElementById("btn-changeGreen1").disabled = true;
+    document.getElementById("Hint01").disabled = true;
+}
+
+
+// NUMBER 2
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Person or organization";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain02").innerHTML = "Wrong! C. Terminated State is reached when a process completes its execution or terminates by the operating system.";
+    
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "Nice! C. Terminated State is reached when a process completes its execution or terminates by the operating system.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+//NUMBER 3
+function Hint3() {
+    document.getElementById("Explain03").innerHTML = "Support language for software developers";
+}
+
+function ExplainWrong3() {
+    document.getElementById("Explain03").innerHTML = "Not that, it's A. PCB Process Control Block in OS (PCB) is a data structure used by the operating system to manage information about a process.";
+    
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
+    document.getElementById("Hint03").disabled = true;
+}
+
+function ExplainCorrect3() {
+    document.getElementById("Explain03").innerHTML = "Amazing! A. PCB Process Control Block in OS (PCB) is a data structure used by the operating system to manage information about a process.";
+    
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
+    document.getElementById("Hint03").disabled = true;
+}
+
+//NUMBER 4
+function Hint4() {
+    document.getElementById("Explain04").innerHTML = "Language used to add layout and visual effects to HTML elements";
+}
+
+function ExplainWrong4() {
+    document.getElementById("Explain04").innerHTML = "Not the correct one, it's A. Data is defined as programs or text and for this discussion is broken down into two classes: System data, which makes up the operating system and its extensions. ";
+    
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect4() {
+    document.getElementById("Explain02").innerHTML = "Yeah! A. Data is defined as programs or text and for this discussion is broken down into two classes: System data, which makes up the operating system and its extensions. ";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 5
+function Hint5() {
+    document.getElementById("Explain05").innerHTML = "Mark Zuckerberg";
+}
+
+function ExplainWrong5() {
+    document.getElementById("Explain05").innerHTML = "No! B. Pointer are memory addresses of data objects in the operating system kernel or in the address space of a user process.";
+    
+    document.getElementById("btn-changeRed5").className = "btn btn-danger";
+    document.getElementById("btn-changeRed05").className = "btn btn-danger";
+    document.getElementById("btn-changeRed005").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen5").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed5").disabled = true;
+    document.getElementById("btn-changeRed05").disabled = true;
+    document.getElementById("btn-changeRed005").disabled = true;
+    document.getElementById("btn-changeGreen5").disabled = true;
+    document.getElementById("Hint05").disabled = true;
+}
+
+function ExplainCorrect5() {
+    document.getElementById("Explain05").innerHTML = "Nice one! B. Pointer are memory addresses of data objects in the operating system kernel or in the address space of a user process.";
+    
+    document.getElementById("btn-changeGreen5").className = "btn btn-success";
+    document.getElementById("btn-changeRed5").className = "btn btn-danger";
+    document.getElementById("btn-changeRed05").className = "btn btn-danger";
+    document.getElementById("btn-changeRed005").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed5").disabled = true;
+    document.getElementById("btn-changeRed05").disabled = true;
+    document.getElementById("btn-changeRed005").disabled = true;
+    document.getElementById("btn-changeGreen5").disabled = true;
+    document.getElementById("Hint05").disabled = true;
+}
+
+//NUMBER 6
+function Hint6() {
+    document.getElementById("Explain06").innerHTML = "Process of planning, designing, creating, testing, and deploying";
+}
+
+function ExplainWrong6() {
+    document.getElementById("Explain06").innerHTML = "Nope, it's A. Blocked State is a process transitions to a blocked state when it is waiting for some event, such as a resource becoming available or the completion of an I/O operation.";
+    
+    document.getElementById("btn-changeRed6").className = "btn btn-danger";
+    document.getElementById("btn-changeRed06").className = "btn btn-danger";
+    document.getElementById("btn-changeRed006").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen6").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed6").disabled = true;
+    document.getElementById("btn-changeRed06").disabled = true;
+    document.getElementById("btn-changeRed006").disabled = true;
+    document.getElementById("btn-changeGreen6").disabled = true;
+    document.getElementById("Hint06").disabled = true;
+}
+
+function ExplainCorrect6() {
+    document.getElementById("Explain06").innerHTML = "That's the one! A. Blocked State is a process transitions to a blocked state when it is waiting for some event, such as a resource becoming available or the completion of an I/O operation.";
+    
+    document.getElementById("btn-changeGreen6").className = "btn btn-success";
+    document.getElementById("btn-changeRed06").className = "btn btn-danger";
+    document.getElementById("btn-changeRed6").className = "btn btn-danger";
+    document.getElementById("btn-changeRed006").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed6").disabled = true;
+    document.getElementById("btn-changeRed06").disabled = true;
+    document.getElementById("btn-changeRed006").disabled = true;
+    document.getElementById("btn-changeGreen6").disabled = true;
+    document.getElementById("Hint06").disabled = true;
+}
+
+//NUMBER 7
+function Hint7() {
+    document.getElementById("Explain07").innerHTML = "Executed by tapping";
+}
+
+function ExplainWrong7() {
+    document.getElementById("Explain07").innerHTML = "Wrong... A. Terminated State reached when a process completes its execution or terminates by the operating system. In this state, the process no longer uses any system resources, and its memory space is deallocated.";
+    
+    document.getElementById("btn-changeRed7").className = "btn btn-danger";
+    document.getElementById("btn-changeRed07").className = "btn btn-danger";
+    document.getElementById("btn-changeRed007").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen7").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed7").disabled = true;
+    document.getElementById("btn-changeRed07").disabled = true;
+    document.getElementById("btn-changeRed007").disabled = true;
+    document.getElementById("btn-changeGreen7").disabled = true;
+    document.getElementById("Hint07").disabled = true;
+}
+
+function ExplainCorrect7() {
+    document.getElementById("Explain07").innerHTML = "Nice one! A. Terminated State reached when a process completes its execution or terminates by the operating system. In this state, the process no longer uses any system resources, and its memory space is deallocated.";
+    
+    document.getElementById("btn-changeGreen7").className = "btn btn-success";
+    document.getElementById("btn-changeRed7").className = "btn btn-danger";
+    document.getElementById("btn-changeRed07").className = "btn btn-danger";
+    document.getElementById("btn-changeRed007").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed7").disabled = true;
+    document.getElementById("btn-changeRed07").disabled = true;
+    document.getElementById("btn-changeRed007").disabled = true;
+    document.getElementById("btn-changeGreen7").disabled = true;
+    document.getElementById("Hint07").disabled = true;
+}
+
+//NUMBER 8
+function Hint8() {
+    document.getElementById("Explain08").innerHTML = "Can be used offline";
+}
+
+function ExplainWrong8() {
+    document.getElementById("Explain08").innerHTML = "Too bad! It's D. Fatal is a type of error that forces a computer program to close or the entire operating system to shut down suddenly.";
+    
+    document.getElementById("btn-changeRed8").className = "btn btn-danger";
+    document.getElementById("btn-changeRed08").className = "btn btn-danger";
+    document.getElementById("btn-changeRed008").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen8").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed8").disabled = true;
+    document.getElementById("btn-changeRed08").disabled = true;
+    document.getElementById("btn-changeRed008").disabled = true;
+    document.getElementById("btn-changeGreen8").disabled = true;
+    document.getElementById("Hint08").disabled = true;
+}
+
+function ExplainCorrect8() {
+    document.getElementById("Explain08").innerHTML = "Yey! D. Fatal is a type of error that forces a computer program to close or the entire operating system to shut down suddenly.";
+    
+    document.getElementById("btn-changeGreen8").className = "btn btn-success";
+    document.getElementById("btn-changeRed8").className = "btn btn-danger";
+    document.getElementById("btn-changeRed08").className = "btn btn-danger";
+    document.getElementById("btn-changeRed008").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed8").disabled = true;
+    document.getElementById("btn-changeRed08").disabled = true;
+    document.getElementById("btn-changeRed008").disabled = true;
+    document.getElementById("btn-changeGreen8").disabled = true;
+    document.getElementById("Hint08").disabled = true;
+}
+
+//NUMBER 9
+function Hint9() {
+    document.getElementById("Explain09").innerHTML = "Process of planning, designing, creating, testing, and deploying";
+}
+
+function ExplainWrong9() {
+    document.getElementById("Explain09").innerHTML = "Err nope! B. Service responsible for the management of platform resources, including the processor, memory, files, and input and output.";
+    
+    document.getElementById("btn-changeRed9").className = "btn btn-danger";
+    document.getElementById("btn-changeRed09").className = "btn btn-danger";
+    document.getElementById("btn-changeRed009").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen9").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed9").disabled = true;
+    document.getElementById("btn-changeRed09").disabled = true;
+    document.getElementById("btn-changeRed009").disabled = true;
+    document.getElementById("btn-changeGreen9").disabled = true;
+    document.getElementById("Hint09").disabled = true;
+}
+
+function ExplainCorrect9() {
+    document.getElementById("Explain09").innerHTML = "You are Correct! B. Service responsible for the management of platform resources, including the processor, memory, files, and input and output.";
+    
+    document.getElementById("btn-changeGreen9").className = "btn btn-success";
+    document.getElementById("btn-changeRed9").className = "btn btn-danger";
+    document.getElementById("btn-changeRed09").className = "btn btn-danger";
+    document.getElementById("btn-changeRed009").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed9").disabled = true;
+    document.getElementById("btn-changeRed09").disabled = true;
+    document.getElementById("btn-changeRed009").disabled = true;
+    document.getElementById("btn-changeGreen9").disabled = true;
+    document.getElementById("Hint09").disabled = true;
+}
+
+//NUMBER 10
+function Hint10() {
+    document.getElementById("Explain010").innerHTML = "Official online store for digital media";
+}
+
+function ExplainWrong10() {
+    document.getElementById("Explain010").innerHTML = "Nah nah! A. Register a type of computer memory built directly into the processor or CPU (Central Processing Unit) that is used to store and manipulate data during the execution of instructions. ";
+    
+    document.getElementById("btn-changeRed10").className = "btn btn-danger";
+    document.getElementById("btn-changeRed010").className = "btn btn-danger";
+    document.getElementById("btn-changeRed0010").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen10").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed10").disabled = true;
+    document.getElementById("btn-changeRed010").disabled = true;
+    document.getElementById("btn-changeRed0010").disabled = true;
+    document.getElementById("btn-changeGreen10").disabled = true;
+    document.getElementById("Hint010").disabled = true;
+}
+
+function ExplainCorrect10() {
+    document.getElementById("Explain010").innerHTML = "You got it! A. Register a type of computer memory built directly into the processor or CPU (Central Processing Unit) that is used to store and manipulate data during the execution of instructions. ";
+    
+    document.getElementById("btn-changeGreen10").className = "btn btn-success";
+    document.getElementById("btn-changeRed10").className = "btn btn-danger";
+    document.getElementById("btn-changeRed010").className = "btn btn-danger";
+    document.getElementById("btn-changeRed0010").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed10").disabled = true;
+    document.getElementById("btn-changeRed010").disabled = true;
+    document.getElementById("btn-changeRed0010").disabled = true;
+    document.getElementById("btn-changeGreen10").disabled = true;
+    document.getElementById("Hint010").disabled = true;
+}
