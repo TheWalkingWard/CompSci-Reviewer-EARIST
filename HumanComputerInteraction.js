9cfeb5de599ea7e4ab63d59abeec09a0a3d37cf6
Jahred-Mario <jahredbaccay97@gmail.com>
2023-12-06 09:54:42 +0800

    SubChoices updated

--- HumanComputerInteraction.js
+++ HumanComputerInteraction.js
@@ -67,11 +67,11 @@ function ExplainCorrect2() {
 }
 
 //NUMBER 3
-function Hint2() {
+function Hint3() {
     document.getElementById("Explain03").innerHTML = "Close ni Baccay yun";
 }
 
-function ExplainWrong2() {
+function ExplainWrong3() {
     document.getElementById("Explain03").innerHTML = "Wrong Answer\n The Correct is\n C. Python is often used as a support language for software developers, for build control and management, testing, and in many other ways.";
     document.getElementById("btn-changeRed3").className = "btn btn-danger";
     document.getElementById("btn-changeRed03").className = "btn btn-danger";
@@ -85,7 +85,7 @@ function ExplainWrong2() {
     document.getElementById("Hint03").disabled = true;
 }
 
-function ExplainCorrect2() {
+function ExplainCorrect3() {
     document.getElementById("Explain03").innerHTML = "C. Python is often used as a support language for software developers, for build control and management, testing, and in many other ways.";
     
     document.getElementById("btn-changeGreen3").className = "btn btn-success";
@@ -97,39 +97,243 @@ function ExplainCorrect2() {
     document.getElementById("btn-changeRed03").disabled = true;
     document.getElementById("btn-changeRed003").disabled = true;
     document.getElementById("btn-changeGreen3").disabled = true;
-    document.getElementById("Hint02").disabled = true;
+    document.getElementById("Hint03").disabled = true;
 }
 
 //NUMBER 4
-function Hint2() {
-    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+function Hint4() {
+    document.getElementById("Explain04").innerHTML = "Close ni Baccay yun";
 }
 
-function ExplainWrong2() {
-    document.getElementById("Explain02").innerHTML = "Wrong Answer\n The Correct is\n";
-    document.getElementById("btn-changeRed2").className = "btn btn-danger";
-    document.getElementById("btn-changeRed02").className = "btn btn-danger";
-    document.getElementById("btn-changeRed002").className = "btn btn-danger";
-    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+function ExplainWrong4() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
 
-    document.getElementById("btn-changeRed2").disabled = true;
-    document.getElementById("btn-changeRed02").disabled = true;
-    document.getElementById("btn-changeRed002").disabled = true;
-    document.getElementById("btn-changeGreen2").disabled = true;
-    document.getElementById("Hint02").disabled = true;
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
 }
 
-function ExplainCorrect2() {
-    document.getElementById("Explain02").innerHTML = "Nice! Si Usog nga!";
+function ExplainCorrect4() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
     
     document.getElementById("btn-changeGreen2").className = "btn btn-success";
     document.getElementById("btn-changeRed2").className = "btn btn-danger";
     document.getElementById("btn-changeRed02").className = "btn btn-danger";
     document.getElementById("btn-changeRed002").className = "btn btn-danger";
 
-    document.getElementById("btn-changeRed2").disabled = true;
-    document.getElementById("btn-changeRed02").disabled = true;
-    document.getElementById("btn-changeRed002").disabled = true;
-    document.getElementById("btn-changeGreen2").disabled = true;
-    document.getElementById("Hint02").disabled = true;
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 5
+function Hint2() {
+    document.getElementById("Explain05").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong5() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n D. Facebook is developed by Mark Elliot Zuckerberg.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect5() {
+    document.getElementById("Explain02").innerHTML = "D. Facebook is developed by Mark Elliot Zuckerberg.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 6
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 7
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 8
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 9
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+//NUMBER 10
+function Hint2() {
+    document.getElementById("Explain02").innerHTML = "Close ni Baccay yun";
+}
+
+function ExplainWrong2() {
+    document.getElementById("Explain04").innerHTML = "Wrong Answer\n The Correct is\n B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    document.getElementById("btn-changeRed4").className = "btn btn-danger";
+    document.getElementById("btn-changeRed04").className = "btn btn-danger";
+    document.getElementById("btn-changeRed004").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen4").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
+}
+
+function ExplainCorrect2() {
+    document.getElementById("Explain02").innerHTML = "B. Cascading Style Sheets it's a stylesheet language used to add layout and visual effects to HTML elements. CSS is used specifically for markup languages, in this case HTML.";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed4").disabled = true;
+    document.getElementById("btn-changeRed04").disabled = true;
+    document.getElementById("btn-changeRed004").disabled = true;
+    document.getElementById("btn-changeGreen4").disabled = true;
+    document.getElementById("Hint04").disabled = true;
 }
