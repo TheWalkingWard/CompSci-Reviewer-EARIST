40e4a7b05fad2ff0f42a0563b4a90513017967ff
Jahred-Mario <jahredbaccay97@gmail.com>
2024-02-03 23:08:51 +0800

    Manginsay turn

--- Algorithm and Complexity.js
+++ Algorithm and Complexity.js
@@ -1,9 +1,9 @@
 function Hint1() {
-    document.getElementById("Explain01").innerHTML = "Example Hint";
+    document.getElementById("Explain01").innerHTML = "contiguous and adjacent memory locations";
 }
 
 function ExplainWrong1() {
-    document.getElementById("Explain01").innerHTML = "Wrong Answer showing";
+    // document.getElementById("Explain01").innerHTML = "Wrong Answer showing";
     document.getElementById("btn-changeRed1").className = "btn btn-danger";
     document.getElementById("btn-changeRed01").className = "btn btn-danger";
     document.getElementById("btn-changeRed001").className = "btn btn-danger";
@@ -17,7 +17,7 @@ function ExplainWrong1() {
 }
 
 function ExplainCorrect1() {
-    document.getElementById("Explain01").innerHTML = "Sample Correct Answer";
+    // document.getElementById("Explain01").innerHTML = "Sample Correct Answer";
     
     document.getElementById("btn-changeGreen1").className = "btn btn-success";
     document.getElementById("btn-changeRed1").className = "btn btn-danger";
@@ -34,11 +34,11 @@ function ExplainCorrect1() {
 
 
 function Hint2() {
-    document.getElementById("Explain02").innerHTML = "Hint 2";
+    document.getElementById("Explain02").innerHTML = "current element in a sequence";
 }
 
 function ExplainWrong2() {
-    document.getElementById("Explain02").innerHTML = "Wrong Ans 2";
+    // document.getElementById("Explain02").innerHTML = "Wrong Ans 2";
     document.getElementById("btn-changeRed2").className = "btn btn-danger";
     document.getElementById("btn-changeRed02").className = "btn btn-danger";
     document.getElementById("btn-changeRed002").className = "btn btn-danger";
@@ -52,7 +52,43 @@ function ExplainWrong2() {
 }
 
 function ExplainCorrect2() {
-    document.getElementById("Explain02").innerHTML = "Correct Ans 2";
+    // document.getElementById("Explain02").innerHTML = "Correct Ans 2";
+    
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+
+
+
+function Hint3() {
+    document.getElementById("Explain03").innerHTML = "Hint 3";
+}
+
+function ExplainWrong2() {
+    // document.getElementById("Explain02").innerHTML = "Wrong Ans 2";
+    document.getElementById("btn-changeRed2").className = "btn btn-danger";
+    document.getElementById("btn-changeRed02").className = "btn btn-danger";
+    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+
+    document.getElementById("btn-changeRed2").disabled = true;
+    document.getElementById("btn-changeRed02").disabled = true;
+    document.getElementById("btn-changeRed002").disabled = true;
+    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("Hint02").disabled = true;
+}
+
+function ExplainCorrect2() {
+    // document.getElementById("Explain02").innerHTML = "Correct Ans 2";
     
     document.getElementById("btn-changeGreen2").className = "btn btn-success";
     document.getElementById("btn-changeRed2").className = "btn btn-danger";
