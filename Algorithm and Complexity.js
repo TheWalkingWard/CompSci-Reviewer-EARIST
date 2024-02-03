d542cfc026b12fd8bea05accefbfe0a8f0e51340
Jahred-Mario <49589939+Jahred-Mario@users.noreply.github.com>
2024-02-03 23:18:07 +0800

    New Sub

--- Algorithm and Complexity.js
+++ Algorithm and Complexity.js
@@ -70,34 +70,34 @@ function ExplainCorrect2() {
 
 
 function Hint3() {
-    document.getElementById("Explain03").innerHTML = "Hint 3";
+    document.getElementById("Explain03").innerHTML = "defined as a collection of nodes";
 }
 
-function ExplainWrong2() {
+function ExplainWrong3() {
     // document.getElementById("Explain02").innerHTML = "Wrong Ans 2";
-    document.getElementById("btn-changeRed2").className = "btn btn-danger";
-    document.getElementById("btn-changeRed02").className = "btn btn-danger";
-    document.getElementById("btn-changeRed002").className = "btn btn-danger";
-    document.getElementById("btn-changeGreen2").className = "btn btn-success";
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
 
-    document.getElementById("btn-changeRed2").disabled = true;
-    document.getElementById("btn-changeRed02").disabled = true;
-    document.getElementById("btn-changeRed002").disabled = true;
-    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
     document.getElementById("Hint02").disabled = true;
 }
 
-function ExplainCorrect2() {
+function ExplainCorrect3() {
     // document.getElementById("Explain02").innerHTML = "Correct Ans 2";
     
-    document.getElementById("btn-changeGreen2").className = "btn btn-success";
-    document.getElementById("btn-changeRed2").className = "btn btn-danger";
-    document.getElementById("btn-changeRed02").className = "btn btn-danger";
-    document.getElementById("btn-changeRed002").className = "btn btn-danger";
+    document.getElementById("btn-changeGreen3").className = "btn btn-success";
+    document.getElementById("btn-changeRed3").className = "btn btn-danger";
+    document.getElementById("btn-changeRed03").className = "btn btn-danger";
+    document.getElementById("btn-changeRed003").className = "btn btn-danger";
 
-    document.getElementById("btn-changeRed2").disabled = true;
-    document.getElementById("btn-changeRed02").disabled = true;
-    document.getElementById("btn-changeRed002").disabled = true;
-    document.getElementById("btn-changeGreen2").disabled = true;
+    document.getElementById("btn-changeRed3").disabled = true;
+    document.getElementById("btn-changeRed03").disabled = true;
+    document.getElementById("btn-changeRed003").disabled = true;
+    document.getElementById("btn-changeGreen3").disabled = true;
     document.getElementById("Hint02").disabled = true;
 }
